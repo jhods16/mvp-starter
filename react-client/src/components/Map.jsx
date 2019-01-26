18d7d74ead8bbccf3cc05f98f2ac9409251f2bb7
Jessica <jessicarhodson@gmail.com>
2019-01-26 15:30:44 -0800

    Build click handlers according to api

--- react-client/src/components/Map.jsx
+++ react-client/src/components/Map.jsx
@@ -5,11 +5,42 @@ import api_key from '../../../maps.config.js';
 export class MapContainer extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      showInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+    }
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.closeInfoWindow = this.closeInfoWindow.bind(this);
+  }
+
+  onMarkerClick(props, marker, e) {
+    this.setState({
+      activeMarker: marker,
+      selectedPlace: props,
+      showInfoWindow: true,
+    })
+  }
+
+  closeInfoWindow() {
+    this.setState({
+      showInfoWindow: false,
+    })
   }
 
   render() {
     return (
-      <Map google={this.props.google}>
+      <Map style={{width: '95%', height: '100%', margin: 'auto'}} google={this.props.google} onClick={this.closeInfoWindow}>
+        <InfoWindow
+          visible={this.state.showInfoWindow}
+          marker={this.state.activeMarker}
+          onClick={() => {console.log('click')}}
+        >
+          <div>
+            <h3>Current Location</h3>
+          </div>
+        </InfoWindow>
+        <Marker onClick={this.onMarkerClick}></Marker>
       </Map>
     )
   }
