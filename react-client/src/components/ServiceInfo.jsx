c0e4922f0909ae8aeba9eacf0915c2509c3c8964
Jessica <jessicarhodson@gmail.com>
2019-01-28 12:41:29 -0800

    Move key to react component from index.html

--- react-client/src/components/ServiceInfo.jsx
+++ react-client/src/components/ServiceInfo.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import InfoWindow from 'google-maps-react';
 
-const ServiceInfo = (props) => (
-  <div>
-    { props.service.name }
-  </div>
-)
+const ServiceInfo = (props) => {
+  return (
+    <ul className='key'>
+      <li onClick={props.selectByService.bind(this)} value='Needle Exchange'><img src='./images/squat-marker-cyan.svg'></img>Needle Exchanges & Drop Off</li>
+      <li onClick={props.selectByService.bind(this)} value='Narcan'><img src='./images/squat-marker-crimson.svg'></img>Narcan/Naloxone Access</li>
+      <li onClick={props.selectByService.bind(this)} value='Methadone'><img src='./images/squat-marker-green.svg'></img>Methadone Clinic</li>
+      <li onClick={props.selectByService.bind(this)} value='Suboxone'><img src='./images/squat-marker-yellow.svg'></img>Suboxone Clinc</li>
+      <li onClick={props.selectByService.bind(this)} value='NA Meeting'><img src='./images/squat-marker-purple.svg'></img>NA/AA Meeting</li>
+      <li onClick={props.selectByService.bind(this)} value='Other Meeting'><img src='./images/squat-marker-orange.svg'></img>Other Support Meeting</li>
+    </ul>
+  )
+}
+
 
 export default ServiceInfo;
