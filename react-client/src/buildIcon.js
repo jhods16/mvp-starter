bbb09d8d15b3327bd5be125a4df50db94b310b8c
Jessica <jessicarhodson@gmail.com>
2019-01-28 11:47:00 -0800

    Change icons to colored map symbols

--- react-client/src/buildIcon.js
+++ react-client/src/buildIcon.js
@@ -1,21 +1,21 @@
 const iconBuilder = (type) => {
   let icon = {
-    url: "https://upload.wikimedia.org/wikipedia/commons/8/89/Map_symbol_location_02.png",
-    anchor: new google.maps.Point(40,40),
+    url: './squat-marker-cyan.svg',
+    anchor: new google.maps.Point(0,0),
     scaledSize: new google.maps.Size(40,40)
   }
 
   if (type === 'Narcan') {
-    icon.url = 'https://upload.wikimedia.org/wikipedia/commons/f/f4/Font_Awesome_5_solid_heartbeat.svg'
+    icon.url = './images/squat-marker-crimson.svg'
   }
   if (type === 'Methadone') {
-    icon.url = 'https://cdn.pixabay.com/photo/2014/04/03/11/07/pharmacy-311773_1280.png'
+    icon.url = './images/squat-marker-green.svg'
   }
   if (type === 'Suboxone') {
-    icon.url = 'https://upload.wikimedia.org/wikipedia/commons/a/a2/Rx_symbol_border.svg'
+    icon.url = './images/squat-marker-yellow.svg'
   }
   if (type === 'NA Meeting') {
-    icon.url = 'https://upload.wikimedia.org/wikipedia/commons/3/37/People_icon.svg'
+    icon.url = './images/squat-marker-purple.svg'
   }
   return icon;
 };
