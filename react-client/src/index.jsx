ec54de57e012fb1e84b741786195b8d5e80a1bba
Jessica <jessicarhodson@gmail.com>
2019-01-28 12:41:51 -0800

    Filter by clicking on key items

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,13 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import MapContainer from './components/Map.jsx';
+import ServiceInfo from './components/ServiceInfo.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      services: []
+      services: [],
+      unfilteredServices: [],
+      serviceType: '',
     }
+    this.selectByService = this.selectByService.bind(this);
+  }
+
+  selectByService(e) {
+    let type = e.target.getAttribute('value');
+    let filteredServices;
+    let unfilteredServices = this.state.unfilteredServices;
+
+    console.log(this.state.unfilteredServices)
+    if (this.state.unfilteredServices === this.state.services) {
+      filteredServices = this.state.services.filter((service) => {
+        return service.type === type;
+      });
+      this.setState({
+        services: filteredServices
+      });
+    } else {
+      this.setState({
+        services: unfilteredServices
+      }, () => {
+        filteredServices = this.state.services.filter((service) => {
+          return service.type === type;
+        });
+        this.setState({
+          services: filteredServices
+        });
+      })
+    }
+
+    
   }
 
   componentDidMount() {
@@ -16,7 +49,8 @@ class App extends React.Component {
       url: '/services', 
       success: (data) => {
         this.setState({
-          services: data
+          services: data,
+          unfilteredServices: data,
         })
       },
       error: (err) => {
@@ -28,7 +62,7 @@ class App extends React.Component {
   render () {
     return (
     <div>
-      {/* <List items={this.state.items}/> */}
+      <ServiceInfo selectByService={this.selectByService}/>
       <MapContainer services={this.state.services}></MapContainer>
     </div>)
   }
