c454cea8e4b2b1e0369b0f06f7eb076996535e30
Jessica <jessicarhodson@gmail.com>
2019-01-29 10:39:11 -0800

    Style addResource form

--- react-client/src/components/AddResource.jsx
+++ react-client/src/components/AddResource.jsx
@@ -51,41 +51,41 @@ class AddResource extends React.Component {
   
   render() {
     return (
-      <form onSubmit={this.submitForm}>
-        <h2>Add a Resource</h2><br></br>
+      <form onSubmit={this.submitForm} style={{marginLeft: '50px'}}>
+        <h2 style={{marginBottom: '0px'}}>Add a Resource</h2><br></br>
         Name:
-        <input type='text' name='name' onChange={this.handleChange.bind(this)}></input><br></br>
-        Type of Service:
-        <select name='type' onChange={this.handleChange.bind(this)}>
-          <option value='Subxone'>Subxone Clinic</option>
+        <input style={{marginLeft: '33px'}} className='form' type='text' name='name' onChange={this.handleChange.bind(this)}></input><br></br>
+        Type:
+        <select style={{marginLeft: '33px'}} className='form' name='type' onChange={this.handleChange.bind(this)}>
+          <option className='option' value='Subxone'>Suboxone Clinic</option>
           <option value='Methadone'>Methadone Clinic</option>
           <option value='Narcan'>Narcan Dispensing</option>
-          <option value='Needle Exchange'>Needle Exchange/Drop Off</option>
+          <option value='Needle Exchange'>Needle Exchange</option>
           <option value='NA Meeting'>NA/AA Meeting</option>
           <option value='Other Meeting'>Other Support Meeting</option>
         </select><br></br>
         Address: 
-        <input type='text' name='location' onChange={this.handleChange.bind(this)}></input><br></br>
+        <input style={{marginLeft: '10px'}} className='form' type='text' name='location' onChange={this.handleChange.bind(this)}></input><br></br>
         Phone: 
-        <input type='text' name='phone' onChange={this.handleChange.bind(this)}></input><br></br>
+        <input style={{marginLeft: '26px'}} className='form' type='text' name='phone' onChange={this.handleChange.bind(this)}></input><br></br>
         Email: 
-        <input type='text' name='email' onChange={this.handleChange.bind(this)}></input><br></br>
+        <input style={{marginLeft: '26px'}} className='form' type='text' name='email' onChange={this.handleChange.bind(this)}></input><br></br>
         Website: 
-        <input type='text' name='website' onChange={this.handleChange.bind(this)}></input><br></br>
+        <input style={{marginBottom: '6px', marginLeft: '10px'}} className='form' type='text' name='website' onChange={this.handleChange.bind(this)}></input><br></br>
         Insurance Accepted:  <br></br>
-        <input type='checkbox' name='insurance' onChange={this.handleChange.bind(this)} value='Blue Shield'/>Blue Shield<br></br>
-        <input type='checkbox' name='insurance' onChange={this.handleChange.bind(this)} value='UnitedHealth Group'/>UnitedHealth Group<br></br>
-        <input type='checkbox' name='insurance' onChange={this.handleChange.bind(this)} value='Anthem'/>Anthem<br></br>
-        <input type='checkbox' name='insurance' onChange={this.handleChange.bind(this)} value='Aetna'/>Aetna<br></br>
-        <input type='checkbox' name='insurance' onChange={this.handleChange.bind(this)} value='Cigna'/>Cigna<br></br>
-        <input type='checkbox' name='insurance' onChange={this.handleChange.bind(this)} value='Medicare'/>Medicare<br></br>
-        <input type='checkbox' name='insurance' onChange={this.handleChange.bind(this)} value='Medi-cal'/>Medi-cal<br></br>
+        <input style={{marginTop: '10px'}} className='form' type='checkbox' name='insurance' onChange={this.handleChange.bind(this)} value='Blue Shield'/>Blue Shield<br></br>
+        <input className='form' type='checkbox' name='insurance' onChange={this.handleChange.bind(this)} value='UnitedHealth Group'/>UnitedHealth Group<br></br>
+        <input className='form' type='checkbox' name='insurance' onChange={this.handleChange.bind(this)} value='Anthem'/>Anthem<br></br>
+        <input className='form' type='checkbox' name='insurance' onChange={this.handleChange.bind(this)} value='Aetna'/>Aetna<br></br>
+        <input className='form' type='checkbox' name='insurance' onChange={this.handleChange.bind(this)} value='Cigna'/>Cigna<br></br>
+        <input className='form' type='checkbox' name='insurance' onChange={this.handleChange.bind(this)} value='Medicare'/>Medicare<br></br>
+        <input className='form' type='checkbox' name='insurance' onChange={this.handleChange.bind(this)} value='Medi-cal'/>Medi-cal<br></br>
         Accepting Patients: 
-        <select name='acceptingPts' onChange={this.handleChange.bind(this)}>
+        <select className='form' name='acceptingPts' onChange={this.handleChange.bind(this)}>
           <option value='yes'>yes</option>
           <option value='no'>no</option>
         </select><br></br>
-        <button type='submit'>Add New Resource</button>
+        <button className='form' type='submit'>Add New Resource</button>
       </form>
     )
   }
