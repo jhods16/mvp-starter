a40f17a8e9ad86f7e228f99d9160b4211922d1b3
Jessica <jessicarhodson@gmail.com>
2019-01-28 17:20:17 -0800

    Delete ServiceMarker component

--- react-client/src/components/ServiceMarker.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Marker } from 'google-maps-react';
-
-const ServiceMarker = (props) => (
-  <Marker position={props.service.location}></Marker>
-)
-
-export default ServiceMarker;
